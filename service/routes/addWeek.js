d38275f242e895a82b4bf67b5b83b471efc651d8
womackx <womackx@live.com>
2017-08-05 21:25:24 +0100

    bug

--- service/routes/addWeek.js
+++ service/routes/addWeek.js
@@ -2,4 +2,5 @@ let trainer = require("../models/trainer");
 
 exports.addWeek = function (req, res, next) {
     trainer.findOneAndUpdate({ name: req.name }, { $push: { feedback: req.feedback } });
+    next();
 };
